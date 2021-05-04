578bdf992428690c925cc52fad1eade35b0de913
Eleonora Belova <eleo.belova@yandex.ru>
2021-05-04 14:26:44 +0200

    add all settings and created templates

--- weather/weather_app/views.py
+++ weather/weather_app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+from .forms import CreateUserForm
+
+from django.contrib import messages
+
+
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, "weather/register.html")
+
+
+def index(request):
+    return render(request, 'weather/index.html')
+
+
+def login(request):
+    return render(request, 'weather/login.html')
